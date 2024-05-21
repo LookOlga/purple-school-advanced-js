764730fdc91f0a1f2dbc2a0b55436bdd4dbd30b0
Alex <64134232+LookOlga@users.noreply.github.com>
2024-05-21 20:58:25 +0300

    home work Set

---
+++ app.js
@@ -0,0 +1,15 @@
+
+'use strict'
+
+const arr = [
+    {id: 8, name: 'Вася'},
+    {id: 9, name: 'Света'},
+    {id: 1, name: 'Вася'},
+    {id: 2, name: 'Антон'},
+    {id: 1, name: 'Вася'},
+    {id: 9, name: 'Света'},
+    {id: 5, name: 'Ваня'},
+]
+
+const IDs = [...new Set(arr.map(item => item.id))]
+const res = IDs.map(id => arr.find(item => item.id === id))

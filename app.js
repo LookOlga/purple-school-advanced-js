0325c446318165cb05a4a348b6f7e2fe84a0f655
Alex <64134232+LookOlga@users.noreply.github.com>
2024-05-29 20:28:26 +0300

    Merge changes

--- app.js
+++ app.js
@@ -13,3 +13,29 @@ const arr = [
 
 const IDs = [...new Set(arr.map(item => item.id))]
 const res = IDs.map(id => arr.find(item => item.id === id))
+<<<<<<< Updated upstream
+=======
+
+// Math random task
+
+const throwDice = (dice) => {
+    const diceTypes = ['D4', 'D6', 'D8', 'D10', 'D12', 'D16', 'D20']
+    
+    if(!diceTypes.includes(dice)) return `Unsupported diceType: ${dice}`
+
+    const max = Number(dice.replace(/[a-zA-Z]/g, ''))
+    const min = 1
+
+    return Math.round(Math.random() * (max - min + 1) + min)
+}
+
+
+
+const date = new Date()
+const options = {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric'
+}
+console.log(new Intl.DateTimeFormat('en-US', options).format(date))
+>>>>>>> Stashed changes

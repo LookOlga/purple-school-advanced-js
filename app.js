65140962480aa6576a59d3188ae8b44360f67293
Alex <64134232+LookOlga@users.noreply.github.com>
2024-06-09 22:01:51 +0300

    add fixes

--- app.js
+++ app.js
@@ -1,60 +1,6 @@
 
 'use strict'
 
-const arr = [
-    {id: 8, name: 'Вася'},
-    {id: 9, name: 'Света'},
-    {id: 1, name: 'Вася'},
-    {id: 2, name: 'Антон'},
-    {id: 1, name: 'Вася'},
-    {id: 9, name: 'Света'},
-    {id: 5, name: 'Ваня'},
-]
-
-const IDs = [...new Set(arr.map(item => item.id))]
-const res = IDs.map(id => arr.find(item => item.id === id))
-
-// 4-timer
-
-const setTimer = () => {
-    const timerContainer = document.querySelector('.timer-content')
-    const endDate = new Date('12-31-2024')
-    const endDateMonth = endDate.getMonth() + 1
-   
-    const intervalId = setInterval(() => {
-    const today = new Date();
-    const lastDayOfMonth = new Date(today.getFullYear(), today.getMonth() + 1, 0);
-    const currentDate = new Date(today.getFullYear(), today.getMonth(), today.getDate());
-    const daysLeft = lastDayOfMonth.getDate() - currentDate.getDate();
-        const currentDateParts = new Intl.DateTimeFormat('RU-ru', {
-            month: 'numeric',
-            hour: 'numeric',
-            minute: 'numeric',
-            second: 'numeric'
-        }).formatToParts(today.getTime())
-
-
-        const formattedCurrentParts = currentDateParts.reduce((acc, item) => {
-            acc[item.type] = item.value;
-            return acc;
-        }, {})
-
-        timerContainer.textContent = `${endDateMonth - formattedCurrentParts.month} months 
-        ${daysLeft} days
-        ${24 - formattedCurrentParts.hour - 1} hours
-        ${60 - formattedCurrentParts.minute - 1} minutes
-        ${60 - formattedCurrentParts.second} seconds`
-    }, 1000)
-
-    
-   if(endDate.getTime() === new Date().getTime) {
-    clearInterval(intervalId)
-   }
-   
-}
-
-setTimer()
-
 // 5-oop
 
 const Personage = function ({race, name, language}) {
@@ -67,27 +13,27 @@ Personage.prototype.speak = function() {
     console.log(`${this.name} speaks in ${this.language}`)
 }
 
-const Ork = function({race, name, language, weapon}) {
-    Personage.call(this, {race, name, language});
+const addPersonageAsPrototype = () => Object.create(Personage.prototype)
+
+const Ork = function({weapon, ...properties}) {
+    Personage.call(this, properties)   
     this.weapon = weapon
 }
 
-Ork.prototype = Object.create(Personage.prototype);
+Ork.prototype = addPersonageAsPrototype()
 
 Ork.prototype.beat = function() {
     console.log(`Beat with ${this.weapon}`)
 }
 
-const Elf = function({race, name, language, spellType}) {
-    Personage.call(this, {race, name, language});
+const Elf = function(spellType, ...properties) {
+    Personage.call(this, properties)    
     this.spellType = spellType
 }
+
+Elf.prototype = addPersonageAsPrototype()
 
 Elf.prototype.spell = function() {
     console.log(`Spell ${this.spellType}`)
 }
-
-const ork = new Ork({race: 'Mongolian', name: 'Bork', language: 'English', weapon: 'knife'})
-const elf = new Elf({race: 'China', name: 'Garry', language: 'Chinese', spellType: 'charming'})
-
 

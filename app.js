42b1b581d46b3981a2f011de4088d605b5e75fdd
Alex <64134232+LookOlga@users.noreply.github.com>
2024-06-10 20:18:19 +0300

    remove other code

--- app.js
+++ app.js
@@ -1,19 +1,6 @@
 
 'use strict'
 
-const arr = [
-    {id: 8, name: 'Вася'},
-    {id: 9, name: 'Света'},
-    {id: 1, name: 'Вася'},
-    {id: 2, name: 'Антон'},
-    {id: 1, name: 'Вася'},
-    {id: 9, name: 'Света'},
-    {id: 5, name: 'Ваня'},
-]
-
-const IDs = [...new Set(arr.map(item => item.id))]
-const res = IDs.map(id => arr.find(item => item.id === id))
-
 // 6-class
 
 class Car {

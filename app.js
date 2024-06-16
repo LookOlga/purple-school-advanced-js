631f0b4c3f23a5a50e8b0617f2798bd031ec7b28
Alex <64134232+LookOlga@users.noreply.github.com>
2024-06-16 11:28:16 +0300

    add fixes

--- app.js
+++ app.js
@@ -2,18 +2,10 @@
 
 // 1-map
 
-const arr = [
-    {id: 8, name: 'Вася'},
-    {id: 9, name: 'Света'},
-    {id: 1, name: 'Вася'},
-    {id: 2, name: 'Антон'},
-    {id: 1, name: 'Вася'},
-    {id: 9, name: 'Света'},
-    {id: 5, name: 'Ваня'},
-]
-
-const IDs = [...new Set(arr.map(item => item.id))]
-const res = IDs.map(id => arr.find(item => item.id === id))
+const uniqueseById = (arr) => {
+    const IDs = [...new Set(arr.map(item => item.id))]
+    return IDs.map(id => arr.find(item => item.id === id))
+}
 
 // 2-dice
 
